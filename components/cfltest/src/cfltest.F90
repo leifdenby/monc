862a7b3f099c4fa13b95d75c03a969b646be03e9
toddjones <toddjones@0f676ef4-b20c-4647-9485-21614760d15f>
2020-01-21 09:04:34 +0000

    Fix bug conflicting with dtmmin. Custom merge into /main/branches/dev/toddjones/r7304_tvfff: r7529 cf. r7528 of /main/branches/dev/toddjones/r7137_wiwwiwi@7530
    git-svn-id: https://code.metoffice.gov.uk/svn/monc/main/branches/dev/toddjones/r7304_tvfff@7531 0f676ef4-b20c-4647-9485-21614760d15f

--- components/cfltest/src/cfltest.F90
+++ components/cfltest/src/cfltest.F90
@@ -237,7 +237,7 @@ contains
 
     ! Handle timestep adjustment for time_basis=.true.
     ts_to_next_cfl = current_state%cfl_frequency - mod(current_state%timestep, current_state%cfl_frequency)
-    projected_time = current_state%time + current_state%dtm + (current_state%dtm_new * ts_to_next_cfl)
+    projected_time = current_state%time + current_state%dtm + (current_state%dtm_new * ts_to_next_cfl) + dtmmin
     next_sample_time = min(current_state%next_3d_sample_time, current_state%next_sample_time)
     if ( next_sample_time .gt. 0 .and. projected_time .ge. next_sample_time ) then
       sample_nts = max(1, ceiling( (next_sample_time - (current_state%time + current_state%dtm) ) / current_state%dtm_new ) )
